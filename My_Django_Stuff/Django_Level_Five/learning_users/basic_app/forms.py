ae04ce446f8304e2d3bbd8501528a06934827c71
CpBruceMeena <sandeep10198@gmail.com>
2019-10-28 00:04:14 +0530

    Creating registration form

--- My_Django_Stuff/Django_Level_Five/learning_users/basic_app/forms.py
+++ My_Django_Stuff/Django_Level_Five/learning_users/basic_app/forms.py
@@ -1,8 +1,8 @@
 from django import forms
-from django.contrib.auth.models import Users
+from django.contrib.auth.models import User
 from basic_app.models import UserProfileInfo
 
-def UserForm(forms.ModelForm):
+class UserForm(forms.ModelForm):
     password = forms.CharField(widget = forms.PasswordInput())
 
     class Meta():
